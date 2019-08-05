417c990226042c38a1896846ee4b0434069e14cb
Losses Don <losses.don@gmail.com>
2019-08-05 15:01:53 +0800

    feat: Add @ez-psycho-duteous-bell.

---
+++ server-programs/@ez-psycho-duteous-bell/main.js
@@ -0,0 +1,84 @@
+import { w, i, Delay, Repeat } from '~server';
+import Player from 'play-sound';
+import path from 'path';
+
+const player =  Player();
+
+class RestScreen {
+  constructor(server, manifest) {
+    this.server = server;
+    this.logger = server.logger;
+
+    this.fns = [];
+    this.timer = null;
+
+    if (manifest.times && manifest.times.forEach) {
+      manifest.times.forEach((time) => {
+        this.fns.push({
+          name: `Start ${time}`,
+          fn: this.StartTimer(time).bind(this)
+        });
+      });
+
+      this.fns.push({
+        name: `Kill Timer`,
+        fn: this.killTimer.bind(this)
+      });
+    } else {
+      this.fns.push({
+        name: 'No time config',
+        fn: this.warning.bind(this)
+      })
+    }
+  }
+
+  playSound() {
+    player.play(path.join(__dirname, 'bell.mp3'));
+  }
+
+  warning() {
+    this.logger.log(w('Add some time in the package.json to make this program works.'));
+  }
+
+  StartTimer(time) {
+    return () => this.startTimer(time);
+  }
+
+  async startTimer(time) {
+    this.killTimer(time);
+
+    this.timer = new Repeat((_, count) => {
+      if (this.count !== 3) {
+        this.logger.log(i(`...${3 - count + 1}`, '1s', 3));
+      } else {
+        this.logger.log(i(`Ding~`, '1s', 3));
+      }
+    }, '1s', 4);
+
+    await this.timer.run();
+
+    this.server.broadcast('ST', ['TRG']);
+    this.playSound();
+    this.logger.log(i('Sending start signal to all TRG client.'));
+
+    this.timer = new Delay((
+      () => {
+        this.server.broadcast('EN', ['TRG']);
+        this.playSound();
+        this.logger.log(i('Sending end signal to all TRG client.'));
+      }
+    ).bind(this), time);
+
+    await this.timer.run();
+  }
+
+  killTimer() {
+    if (this.timer && this.timer.kill) {
+      this.timer.kill();
+      this.timer = null;
+      this.logger.log(i(`Killed previous timer.`));
+    }
+  }
+}
+
+export default RestScreen;
